60fba796f2e6acb72815fa1ba2ef68f0dc4314fc
rasim hesenov <devbeyt989@gmail.com>
2022-11-19 21:46:27 +0400

    12 added AOP

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results.Abstract;
 using Core.Utilities.Results.Concrete;
@@ -22,10 +23,10 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
-            ValidationTool.Validate(new ProductValidator(),product);
-
+           
             _productDal.Add(product);
 
             return new SuccessResult(Messages.ProductAdded);
--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -1,6 +1,9 @@
 ﻿using Autofac;
+using Autofac.Extras.DynamicProxy;
 using Business.Abstract;
 using Business.Concrete;
+using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 using System;
@@ -20,19 +23,19 @@ namespace Business.DependencyResolvers.Autofac
             builder.RegisterType<CategoryManager>().As<ICategoryService>().SingleInstance();
             builder.RegisterType<EfCategoryDal>().As<ICategoryDal>().SingleInstance();
 
-        //    builder.RegisterType<UserManager>().As<IUserService>();
-        //    builder.RegisterType<EfUserDal>().As<IUserDal>();
+            //    builder.RegisterType<UserManager>().As<IUserService>();
+            //    builder.RegisterType<EfUserDal>().As<IUserDal>();
 
-        //    builder.RegisterType<AuthManager>().As<IAuthService>();
-        //    builder.RegisterType<JwtHelper>().As<ITokenHelper>();
+            //    builder.RegisterType<AuthManager>().As<IAuthService>();
+            //    builder.RegisterType<JwtHelper>().As<ITokenHelper>();
 
-        //    var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
 
-        //    builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
-        //        .EnableInterfaceInterceptors(new ProxyGenerationOptions()
-        //        {
-        //            Selector = new AspectInterceptorSelector()
-        //        }).SingleInstance();
+            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
+                .EnableInterfaceInterceptors(new ProxyGenerationOptions()
+                {
+                    Selector = new AspectInterceptorSelector()
+                }).SingleInstance();
 
 
         }
